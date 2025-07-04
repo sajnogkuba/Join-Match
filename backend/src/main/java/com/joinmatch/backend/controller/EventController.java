184d4009874fb4ff9b13955a0a49aff2e0c2cbd8
Kuba <jakubpapuda27@gmail.com>
2025-07-04 18:06:03 +0200

    Correct init

--- backend/src/main/java/com/joinmatch/backend/controller/EventController.java
+++ backend/src/main/java/com/joinmatch/backend/controller/EventController.java
@@ -1,6 +1,5 @@
 package com.joinmatch.backend.controller;
 
-import com.joinmatch.backend.model.Event;
 import com.joinmatch.backend.repositories.EventRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
