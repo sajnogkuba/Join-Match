e243cf629d7a2e4c72b326ef3db2a31b10f51cb8
Jakub Sajnóg <jakubsajnog@gmail.com>
2025-07-09 19:57:16 +0200

    Add endpoint connection to featch events from databese to main page

--- frontend/src/Api/types.ts
+++ frontend/src/Api/types.ts
@@ -3,3 +3,19 @@ export interface JwtResponse {
 	refreshToken: string
 	email: string
 }
+
+export interface Event {
+	eventId: number;
+	eventName: string;
+	numberOfParticipants: number;
+	cost: number;
+	ownerId: number;
+	sportObjectName: string;
+	eventVisibilityId: number;
+	status: 'planned' | 'ongoing' | 'finished';
+	scoreTeam1: number | null;
+	scoreTeam2: number | null;
+	eventDate: Date;
+	sportTypeName: string;
+	bookedParticipants: number;
+}
