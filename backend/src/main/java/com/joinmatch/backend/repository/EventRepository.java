f22c0af9ec86b8d18bd860f9ddf6cab72233a3e4
s28490 <s28490@pjwstk.edu.pl>
2025-10-17 11:20:02 +0200

    remove duplicate

--- backend/src/main/java/com/joinmatch/backend/repository/EventRepository.java
+++ backend/src/main/java/com/joinmatch/backend/repository/EventRepository.java
@@ -4,7 +4,6 @@ import com.joinmatch.backend.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
 public interface EventRepository extends JpaRepository<Event, Integer> {
     @Query("SELECT MAX(e.eventId) + 1 FROM Event e")
     Long getNextId();
